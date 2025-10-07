c583ea675eb9f23d757e832ce6010f9125f65c64
Jesus <cautivo.chacon@gmail.com>
2025-10-07 19:42:53 +0200

    wip

--- custom_components/ingeteam_modbus/__init__.py
+++ custom_components/ingeteam_modbus/__init__.py
@@ -175,7 +175,7 @@ class IngeteamModbusHub:
             _LOGGER.warning("Modbus exception occurred while reading data: %s", e)
             return False
         except Exception:
-            _LOGGER.exception("Unexpected error while reading modbus data")
+            _LOGGER.exception("Unexpected error while reading modbus data", e)
             return False
 
     @property
